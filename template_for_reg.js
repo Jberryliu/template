be5d39d8ba0b32eb79a40d3cd23176491d7aad9d
Jberryliu <liuyz0602@163.com>
2015-06-02 15:33:48 +0800

    entire code

--- template_for_reg.js
+++ template_for_reg.js
@@ -1,21 +1,43 @@
 /**
  * Created by berry on 15/6/2.
  */
-var template = function(tpl, data){
+
+/**
+ * var r = [];
+ * r.push();
+ * r.join();
+ */
+var template = function (tpl, data) {
     var re = /<%([^%>]+)?%>/g;  // + 好像有问题
+    var regExp = /^( )?(if|else|for|switch|case|break|continue|{|})(.*)?/g; //{}，感觉不靠谱啊
+    var cursor = 0;
+    var code = 'var r = [];\n';
     var match = [];
+    var add = function (line, js) {
+        js ? (code += line.match(regExp) ? line + '\n' : 'r.push(' + line + ');\n') :
+            (code += 'r.push("' + line.replace(/"/g, '\\"') + '");\n');
+    };
 
-    while(match = re.exec(tpl)){
-        tpl = tpl.replace(match[0], data[match[1]]);
+    while (match = re.exec(tpl)) {
+        add(tpl.slice(cursor, match.index));
+        add(match[1], true);
+        cursor = match.index + match[0].length;
     }
-    return tpl;
-
+    code += 'return r.join("");';
+    console.log(code);
+    return new Function(code.replace(/\t\n\r/g, '')).apply(data);
 };
 
-var tpl = '<h1><%name%> is <%age%> years old.</h1>';
+var tpl = '<h1><%this.name%> is <%this.age.number%> years old.</h1>' +
+    '<h2><%for(var skill in this.skills){%>' +
+    '<span><%this.skills[skill]%></span>' +
+    '<%}%></h2>';
 var data = {
     name: 'berry',
-    age: 28
+    age: {
+        number: 28
+    },
+    skills: ['haha', 'guagua', 'huhu']
 };
 
 console.log(template(tpl, data));
